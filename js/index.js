ce73f0c5beac0b9339071339d6cad4e5d1da32b3
RuthMeryCardenas <ruth.cardenas.perez@gmail.com>
2017-07-18 03:13:01 -0500

    portfolio-Galería de proyectos

--- js/index.js
+++ js/index.js
@@ -1,8 +1,15 @@
-$(window).scroll(function () {
-    var sc = $(window).scrollTop()
-    if (sc > 100) {
-        $("#navbar-main").addClass("navbar-small")
-    } else {
-        $("#navbar-main").removeClass("navbar-small")
-    }
+$().ready( _=> {
+  //Cambiar estilos del navbar al activarse el scroll
+  $(window).scroll(function () {
+      var sc = $(window).scrollTop()
+      if (sc > 100) {
+          $("#navbar-main").addClass("navbar-small")
+      } else {
+          $("#navbar-main").removeClass("navbar-small")
+      }
+  });
+  //Cargar estilos y transiciones de la galería
+  $(function() {
+    $('#dg-container').gallery();
+  });
 });
